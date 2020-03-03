360c6c89cdf5fb8c3d540f33588b8c99b6317067
Entliczekperliczek <ewaperlinska55@gmail.com>
2020-03-03 22:52:27 +0100

    Add user model and router with login and signup eindpoints

---
+++ src/user/model.js
@@ -0,0 +1,12 @@
+const Sequelize = require("sequelize");
+const db = require("../../db");
+
+const User = db.define("user", {
+  username: { type: Sequelize.STRING, allowNull: false },
+  name: { type: Sequelize.STRING, allowNull: false },
+  surname: { type: Sequelize.STRING, allowNull: false },
+  email: { type: Sequelize.STRING, unique: true, allowNull: false },
+  password: { type: Sequelize.STRING, allowNull: false }
+});
+
+module.exports = User;
