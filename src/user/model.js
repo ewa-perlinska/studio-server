755e8a9a14b598716fdf6f71518432c21bb78929
Entliczekperliczek <ewaperlinska55@gmail.com>
2020-03-04 15:22:49 +0100

    Fix Login endpoint

--- src/user/model.js
+++ src/user/model.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const db = require("../../db");
+const db = require("../db");
 
 const User = db.define("user", {
   username: { type: Sequelize.STRING, allowNull: false },
