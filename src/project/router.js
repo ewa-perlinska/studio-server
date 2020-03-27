faaae58ffc779b967730ca78abaca638ed627a74
Matias Garcia <matiasigarcia91@gmail.com>
2020-03-27 12:24:36 +0100

    Create project route

--- src/project/router.js
+++ src/project/router.js
@@ -4,6 +4,7 @@ const Project = require("./model");
 const Studio = require("../studio/model");
 const Image = require("../image/model");
 const router = new Router();
+const _ = require("lodash");
 router.get("/project", async function(request, response, next) {
   try {
     const projects = await Project.findAll();
@@ -22,18 +23,15 @@ router.get("/project/:id", async function(request, response, next) {
     next(error);
   }
 });
-router.post("/studio/:id/project", auth, async function(
-  request,
-  response,
-  next
-) {
+router.post("/studio/:id/project", async function(request, response, next) {
   try {
-    console.log("how my request looks?", request.user.dataValues.id);
+    // console.log("how my request looks?", request.user.dataValues.id);
     console.log("WHAT IS STUDIO ID ", request.params.id);
+    const studioId = 2;
 
-    const studio = await Studio.findByPk(request.params.id);
+    const studio = await Studio.findByPk(studioId);
     if (studio) {
-      request.body.projectDetails.userId = request.user.dataValues.id;
+      request.body.projectDetails.userId = 1;
       console.log("HOW MY REQUEST  BODY LOOOKS ", request.body.projectDetails);
       const newProject = { ...request.body };
       console.log("what is my new !!!!! project ", newProject.projectDetails);
@@ -43,10 +41,11 @@ router.post("/studio/:id/project", auth, async function(
         "what is request.body.image????????",
         newProject.projectDetails.image
       );
-      const projectImages = newProject.projectDetails.image.flat();
+      const projectImages = _.flatten(newProject.projectDetails.image);
       console.log("what is flat project", projectImages);
       await Promise.all(
         projectImages.map(async image => {
+          console.log("creating image:", image);
           return await Image.create({
             image: image,
             projectId: newProject.id
